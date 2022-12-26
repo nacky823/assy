9157ced144e0f7b03214584837c072ff6bf20d59
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2022-12-26 18:50:48 +0900

    fix/ ugoita

--- ros2_smile/talker.py
+++ ros2_smile/talker.py
@@ -1,19 +1,24 @@
 import rclpy
 from  rclpy.node import Node
-from std_msg.msg import Int16
-
-rclpy.init()
-node = Node("talker")
-pub = node.create_publisher(Init16, "countup", 10)
+from std_msgs.msg import Int16
 
 n = 0
+pub =0
+
+def main():
+    global pub
+
+    rclpy.init()
+    node = Node("talker")
+    pub = node.create_publisher(Int16, "/countup", 10)
+
+    node.create_timer(0.5, cb)
+    rclpy.spin(node)
 
 def cb():
     global n
     msg = Int16()
-    msg.date = n
+    msg.data = n
     pub.publish(msg)
     n += 1
-
-node.create_timer(0.5, cb)
 
