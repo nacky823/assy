01ceeb1c4626c5d423cb7c6a2a3a2332f3b5b0fa
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2022-12-26 22:58:20 +0900

    add/ launch

---
+++ launch/talk_listen.launch.py
@@ -0,0 +1,19 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+def generate_launch_description():
+
+    talker = launch_ros.actions.Node(
+        package = 'ros2_smile',
+        executable = 'talker',
+        )
+    listener = launch_ros.actions.Node(
+        package = 'ros_smile',
+        exectable = 'listener',
+        output = 'screen'
+        )
+
+    return launch.LaunchDescription([ talker, listener ])
+
