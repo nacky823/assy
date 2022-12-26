6c635d13993145e71afaf3c7fb78fc4738ff54cc
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2022-12-27 01:10:56 +0900

    change msgs to smile

--- ros2_smile/talker.py
+++ ros2_smile/talker.py
@@ -1,24 +1,21 @@
 import rclpy
 from  rclpy.node import Node
-from std_msgs.msg import Int16
+from smile_msgs.msg import Smile
 
 n = 0
-pub =0
 
-def main():
-    global pub
+rclpy.init()
+node = Node("talker")
+pub = node.create_publisher(Smile, "/smile", 10)
 
-    rclpy.init()
-    node = Node("talker")
-    pub = node.create_publisher(Int16, "/countup", 10)
-
-    node.create_timer(0.5, cb)
-    rclpy.spin(node)
+node.create_timer(0.5, cb)
+rclpy.spin(node)
 
 def cb():
     global n
-    msg = Int16()
-    msg.data = n
+    msg = Smile()
+    msg.name = "joker"
+    msg.age = n
     pub.publish(msg)
     n += 1
 
