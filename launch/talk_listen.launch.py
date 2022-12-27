a19396940206c7d2acd25a8a0adf8104380ccee4
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2022-12-28 03:45:33 +0900

    edit/ launch

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -5,16 +5,17 @@ import launch_ros.actions
 
 def generate_launch_description():
 
-    talker = launch_ros.actions.Node(
-        package = 'ros2_smile',
-        executable = 'talker',
-        output = 'screen'
-        )
     listener = launch_ros.actions.Node(
         package = 'ros2_smile',
         executable = 'listener',
         output = 'screen'
         )
+    talker = launch_ros.actions.Node(
+        package = 'ros2_smile',
+        executable = 'talker',
+        output = 'screen',
+        prefix="xterm -e"
+        )
 
-    return launch.LaunchDescription([ talker, listener ])
+    return launch.LaunchDescription([ listener, talker ])
 
