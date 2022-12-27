560f0482542cfc67e75b202f2480a19f4097ee9a
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2022-12-27 18:52:45 +0900

    edit/ nosou.bash try/ test.yml

--- test/nosou.bash
+++ test/nosou.bash
@@ -7,8 +7,8 @@ dir=~
 cd $dir/ros2_ws
 pwd
 ls -A
-#colcon build
+colcon build
 
-#timeout 10 ros2 launch ros2_smile talk_listen.launch.py > /tmp/ros2_smile.log
-#cat /tmp/ros2_smile.log |
+timeout 5 ros2 launch ros2_smile talk_listen.launch.py > /tmp/ros2_smile.log
+cat /tmp/ros2_smile.log
 #grep 'age=15'
