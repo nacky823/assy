78a1c8e453d3c38ffbd7e4ae4f81215b087cf21e
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2022-12-27 17:16:49 +0900

    pass test.bash

---
+++ test/test.bash
@@ -0,0 +1,15 @@
+#!/bin/bash -i
+echo $-
+
+dir=~
+[ "$1" != "" ] && dir="$1"
+
+cd $dir/ros2_ws
+pwd
+ls -A
+colcon build
+source $dir/.bashrc
+
+timeout 10 ros2 launch ros2_smile talk_listen.launch.py > /tmp/ros2_smile.log
+cat /tmp/ros2_smile.log |
+grep 'age=15'
