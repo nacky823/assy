a542e51c3c5210c10a164846951ac4bfc26980f6
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2022-12-30 04:29:45 +0900

    update

--- ros2_smile/time_zone.py
+++ ros2_smile/time_zone.py
@@ -1,20 +1,24 @@
 import rclpy
 from rclpy.node import Node
 from turtlesim.srv import Spawn
-
+import datetime
 
 class Service():
     def __init__(self, nh):
-        self.srv = nh.create_service(Spawn, "/time_zone", self.cb)
+        srv = nh.create_service(Spawn, "/time_zone", self.cb)
 
     def cb(self, req, res):    
         if req.name == "now":
             res.name = "夜"
         else:
-            res.name = "nolibra"
-    
+            res.name = "Unable to meet request."
+
         return res
 
+    def tz(self):
+        JST_MINUS_UTC = 9
+        now = datetime.timedelta(hours=9)
+        print(now)
 
 
 
@@ -22,6 +26,7 @@ def main():
     rclpy.init()
     node = Node("time_zone")
     service = Service(node)
+    service.tz()
     rclpy.spin(node)
     rclpy.shutdown()
 
