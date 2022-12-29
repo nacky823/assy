64e51b85d09e2499bc2f76b8b53b367677a091fa
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2022-12-29 20:16:59 +0900

    pass class run

--- ros2_smile/tac.py
+++ ros2_smile/tac.py
@@ -4,18 +4,22 @@ from std_msgs.msg import Int16
 
 class Talker():
     def __init__(self,nh):
+        nh.create_timer(0.5, self.cb)
         self.pub = nh.create_publisher(Int16, "/countup", 10)
-        nh.create_timer(0.5, cb)
         self.n = 0
 
-def cb():
-    msg = Int16()
-    msg.data = talker.n
-    talker.pub.publish(msg)
-    print(msg)
-    talker.n += 1
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        print(msg)
+        self.n += 1
 
-rclpy.init()
-node = Node("talker")
-talker = Talker(node)
-rclpy.spin(node)
+def main():
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
+    rclpy.spin(node)
+
+if __name__ == '__main__':
+    main()
