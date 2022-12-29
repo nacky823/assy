06d8701e1b14e81a1745df10e82da8180271a3eb
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2022-12-30 08:20:00 +0900

    update

--- ros2_smile/times.py
+++ ros2_smile/times.py
@@ -4,20 +4,25 @@ from turtlesim.srv import Spawn
 import datetime
 
 class Service():
+
+    MAX_RES_CNT = 1
+    DIFF_UTC = 9
+
     def __init__(self, nh):
+        self.res_cnt = 0
         srv = nh.create_service(Spawn, "/times", self.cb)
 
     def cb(self, req, res):    
         if req.name == "now":
             res.name = self.times()
+            self.res_cnt += 1
         else:
             res.name = "Unable to meet request."
 
         return res
 
     def times(self):
-        JST_MINUS_UTC = 9
-        time_diff = datetime.timedelta(hours=JST_MINUS_UTC)
+        time_diff = datetime.timedelta(hours=self.DIFF_UTC)
         time_zone = datetime.timezone(time_diff)
         now = datetime.datetime.now(time_zone)
         nowhm = now.strftime("%H:%M")
@@ -29,7 +34,9 @@ def main():
     rclpy.init()
     node = Node("time_zone")
     service = Service(node)
-    rclpy.spin(node)
+    while not service.res_cnt == service.MAX_RES_CNT:
+        rclpy.spin_once(node)
+    node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == "__main__":
