f83ea399dab4b0aea3361a6c3a5ff2b44fa4ff06
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2022-12-30 18:04:44 +0900

    add/ selection entry point

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
             "lic = ros2_smile.lic:main",
             "client = ros2_smile.client:main",
             "times = ros2_smile.times:main",
+            "selection = ros2_smile.selection:main",
         ],
     },
 )
