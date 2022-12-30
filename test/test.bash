2f4c561108e7b96c6cd3ce66d855ebc4fd0c4b48
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2022-12-31 07:28:01 +0900

    passed test.bash

--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,6 @@
 #!/bin/bash -i
+# SPDX-FileCopyrightText: 2022 NAGAKI Yuki
+# SPDX-License-Identifier: BSD-3-Clause
 echo $-
 
 dir=~
@@ -10,9 +12,28 @@ ls -A
 colcon build
 echo $?
 source $dir/.bashrc
-
 #timeout 4 ros2 launch ros2_smile srv.launch.py > /tmp/ros2_smile.log
 #cat /tmp/ros2_smile.log |
 #grep 'addtest: 56'
 
+ng () {
+    echo -e "NG at LINE $1"
+    res=1
+    exit $res
+}
 
+res=0
+
+hour=`date +'%H'`
+timeout 2 ros2 run assy times &
+timeout 2 ros2 service call /times turtlesim/srv/Spawn "name: now" > /tmp/ros2_smile.log
+cat /tmp/ros2_smile.log |
+grep 'turtlesim.srv.Spawn_Response(name=' |
+grep $hour
+[ "$?" = 0 ] || ng ${LINENO}
+
+timeout 2 ros2 launch assy ass.launch.py &
+timeout 2 ros2 run assy client > /tmp/ros2_smile.log
+cat /tmp/ros2_smile.log |
+grep '現在の時刻は'
+ee
